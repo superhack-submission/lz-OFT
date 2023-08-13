608ab1cc39885b1afcca9b816950161ce221630f
Vib-UX <vibhav@instaraise.io>
2023-08-13 18:01:44 +0530

    hardhat-test

--- tasks/index.js
+++ tasks/index.js
@@ -1,35 +1,3 @@
-// set the Oracle address for the OmniCounter
-// example:
-task("omniCounterSetOracle", "set the UA (an OmniCounter contract) to use the specified oracle for the destination chain", require("./omniCounterSetOracle"))
-    .addParam("targetNetwork", "the target network name, ie: fuji, or mumbai, etc (from hardhat.config.js)")
-    .addParam("oracle", "the Oracle address for the specified targetNetwork")
-
-// get the Oracle for sending to the destination chain
-task("ocGetOracle", "get the Oracle address being used by the OmniCounter", require("./ocGetOracle")).addParam("targetNetwork", "the target network name, ie: fuji, or mumbai, etc (from hardhat.config.js)")
-
-//
-task("ocPoll", "poll the counter of the OmniCounter", require("./ocPoll"))
-
-//
-task(
-    "omniCounterIncrementWithParamsV1",
-    "increment the destination OmniCounter with gas amount param",
-    require("./omniCounterIncrementWithParamsV1")
-)
-    .addParam("targetNetwork", "the target network name, ie: fuji, or mumbai, etc (from hardhat.config.js)")
-    .addParam("gasAmount", "the gas amount for the destination chain")
-
-//
-task(
-    "omniCounterIncrementWithParamsV2",
-    "increment the destination OmniCounter with gas amount param",
-    require("./omniCounterIncrementWithParamsV2")
-)
-    .addParam("targetNetwork", "the target network name, ie: fuji, or mumbai, etc (from hardhat.config.js)")
-    .addParam("gasAmount", "the gas amount for the destination chain")
-    .addParam("airDropEthQty", "the amount of eth to drop")
-    .addParam("airDropAddr", "the air drop address")
-
 task("routerAddLiquidityETH", "addLiquidityETH to the V2 Router", require("./routerAddLiquidityETH"))
     .addParam("router", "the router address")
     .addParam("token", "the token address")
@@ -41,41 +9,7 @@ task("swapNativeForNative", "swap native on one chain thru StargateComposed to n
     .addParam("dstPoolId", "the poolId to bridge")
     .addParam("qty", "the quanitty of native to swap in")
 
-task("pingPongSetTrustedRemote", "set the trusted remote", require("./pingPongSetTrustedRemote")).addParam(
-    "targetNetwork",
-    "the targetNetwork to set as trusted"
-)
-
-task("ping", "call ping to start the pingPong with the target network", require("./ping")).addParam(
-    "targetNetwork",
-    "the targetNetwork to commence pingponging with"
-)
-
 task("getSigners", "show the signers of the current mnemonic", require("./getSigners")).addOptionalParam("n", "how many to show", 3, types.int)
-
-task("approveERC1155", "approve it to transfer my nfts", require("./approveERC1155")).addParam("addr", "the address to approve")
-
-task("sendProxyONFT1155", "send a tokenid and quantity", require("./sendProxyONFT1155"))
-    .addParam("targetNetwork", "the destination chainId")
-    .addParam("tokenId", "the NFT tokenId")
-    .addParam("quantity", "the quantity of NFT tokenId to send")
-// .addParam("msgValue", "the lz message value, ie: '0.02' ")
-
-task("sendONFT1155", "send a tokenid and quantity", require("./sendONFT1155"))
-    .addParam("targetNetwork", "the destination chainId")
-    .addParam("tokenId", "the NFT tokenId")
-    .addParam("quantity", "the quantity of NFT tokenId to send")
-    .addParam("msgValue", "the lz message value, ie: '0.02' ")
-
-task("batchSendProxyONFT1155", "send a tokenid and quantity", require("./batchSendProxyONFT1155"))
-    .addParam("targetNetwork", "the destination chainId")
-    .addParam("tokenIds", "the NFT tokenId")
-    .addParam("quantities", "the quantity of NFT tokenId to send")
-
-task("batchSendONFT1155", "send a tokenid and quantity", require("./batchSendONFT1155"))
-    .addParam("targetNetwork", "the destination chainId")
-    .addParam("tokenIds", "the NFT tokenId")
-    .addParam("quantities", "the quantity of NFT tokenId to send")
 
 // uint qty,
 // address bridgeToken,                    // the address of the native ERC20 to swap() - *must* be the token for the poolId
@@ -106,7 +40,8 @@ task(
     "setTrustedRemote",
     "setTrustedRemote(chainId, sourceAddr) to enable inbound/outbound messages with your other contracts",
     require("./setTrustedRemote")
-).addParam("targetNetwork", "the target network to set as a trusted remote")
+)
+    .addParam("targetNetwork", "the target network to set as a trusted remote")
     .addOptionalParam("localContract", "Name of local contract if the names are different")
     .addOptionalParam("remoteContract", "Name of remote contract if the names are different")
     .addOptionalParam("contract", "If both contracts are the same name")
@@ -128,44 +63,11 @@ task("oftv2Send", "send tokens to another chain", require("./oftv2Send"))
     .addOptionalParam("contract", "If both contracts are the same name")
 
 //
-task("onftMint", "mint() mint ONFT", require("./onftMint"))
-    .addParam("contract", "Name of contract")
+
+task("verifyContract", "", require("./verifyContract.js")).addParam("contract", "contract name")
 
 //
-task("ownerOf", "ownerOf(tokenId) to get the owner of a token", require("./ownerOf"))
-    .addParam("contract", "Name of contract")
-    .addParam("tokenId", "the tokenId of ONFT")
-
-//
-task("onftSend", "send an ONFT nftId from one chain to another", require("./onftSend"))
-    .addParam("tokenId", "the tokenId of ONFT")
-    .addParam("targetNetwork", "the chainId to transfer to")
-    .addParam("contract", "ONFT contract name")
-
-//
-task("setMinDstGas", "set min gas required on the destination gas", require("./setMinDstGas"))
-    .addParam("packetType", "message Packet type")
-    .addParam("targetNetwork", "the chainId to transfer to")
-    .addParam("contract", "contract name")
-    .addParam("minGas", "min gas")
-
-//
-task("incrementCounter", "increment the destination OmniCounter", require("./incrementCounter"))
-    .addParam("targetNetwork", "the target network name, ie: fuji, or mumbai, etc (from hardhat.config.js)")
-
-// npx hardhat deployWireCheck --e testnet --contract ExampleOFT --proxy-contract ExampleBasedOFT --proxy-chain optimism-kovan
-// npx hardhat deployWireCheck --e testnet --contract ExampleUniversalONFT721
-task("deployWireCheck", "", require("./deployWireCheck"))
-    .addParam("e", "environment testnet/mainet")
-    .addParam("contract", "")
-    .addOptionalParam("proxyChain", "")
-    .addOptionalParam("proxyContract", "")
-
-task("verifyContract", "", require("./verifyContract.js"))
-    .addParam("contract", "contract name")
-
-//
-task("getStoredPayloadEvent", "Detect and clear stored payload", require('./getStoredPayloadEvent'))
+task("getStoredPayloadEvent", "Detect and clear stored payload", require("./getStoredPayloadEvent"))
     .addParam("txStart", "provide a transaction hash in the block you want to start in")
     .addParam("srcAddress", "")
     .addParam("desAddress", "")
@@ -174,7 +76,7 @@ task("getStoredPayloadEvent", "Detect and clear stored payload", require('./getS
     .addOptionalParam("nonce", "nonce to clear")
 
 //
-task("getMessageFailedEvent", "Detect and clear failed message", require('./getMessageFailedEvent'))
+task("getMessageFailedEvent", "Detect and clear failed message", require("./getMessageFailedEvent"))
     .addParam("txStart", "provide a transaction hash in the block you want to start in")
     .addParam("dstUa", "address of dst UA")
     .addOptionalParam("txEnd", "provide a tx hash in the block you want to end at")
